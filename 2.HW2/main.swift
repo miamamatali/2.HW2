1756fdd9d9561d256eba2d45bb3c4749a2b95635
miamamatali <118820793+miamamatali@users.noreply.github.com>
2023-01-17 17:53:49 +0600

    Update main.swift

--- 2.HW2/main.swift
+++ 2.HW2/main.swift
@@ -9,3 +9,101 @@ import Foundation
 
 print("Hello, World!")
 
+class Parents {
+    var name: String
+    var surname: String
+    var age: Int
+    
+    init(name: String, surname: String, age: Int) {
+        self.name = name
+        self.surname = surname
+        self.age = age
+    }
+    func showInfo () {
+        print("name \(name), surname \(surname), age \(age)")
+    }
+}
+    class Dad: Parents {
+        override init(name: String, surname: String, age: Int) {
+            super .init(name: name, surname: surname, age: age)
+        }
+        override func showInfo() {
+            print("name \(name), surname \(surname), age \(age)")
+
+        }
+    }
+class Mom: Parents {
+    override init(name: String, surname: String, age: Int) {
+        super .init(name: name, surname: surname, age: age
+        )
+    }
+    override func showInfo() {
+        print("name \(name), surname \(surname), age \(age)")
+    }
+}
+class Son: Dad {
+    override init(name: String, surname: String, age: Int) {
+        super .init(name: name, surname: surname, age: age)
+    }
+    override func showInfo() {
+        print("name \(name), surname \(surname), age \(age)")
+
+    }
+}
+class Daughter: Mom {
+    override init(name: String, surname: String, age: Int) {
+        super .init(name: name, surname: surname, age: age)
+    }
+    override func showInfo() {
+        print("name \(name), surname \(surname), age \(age)")
+    }
+}
+
+var parents = Parents(name: "Dad", surname: "Fam", age: 40)
+parents.showInfo()
+var father = Dad(name: "Adyl", surname: "Mamatali", age: 39)
+father.showInfo()
+var mother = Mom(name: "Sarah", surname: "Abai", age: 35)
+mother.showInfo()
+var son = Son(name: "David", surname: "Mamatali", age: 19)
+son.showInfo()
+var daughter = Daughter(name: "Mila", surname: "Mamatali", age: 17)
+daughter.showInfo()
+
+class User {
+    var name:String
+    var surname:String
+    init(name: String, surname: String) {
+        self.name = name
+        self.surname = surname
+    }
+    func getFullName () {
+        print("\(name), \(surname)")
+    }
+}
+class Student:User {
+    var year:Int
+    var course:Int
+    init(year: Int, course: Int) {
+        self.year = year
+        self.course = course
+        super .init(name:"", surname:"")
+    }
+    func getCourse () {
+        var thisYear = 2023
+        //создала переменную для того, чтобы отнимать текущий год от года поступления в вуз
+    course == thisYear - year
+        if course >= 5 {
+            print("stduent  course \(course)")
+        } else {
+            print("Был выпущен")
+        }
+        
+    }
+}
+var user = User(name: "fd", surname: "fd")
+user.getFullName()
+var student = Student(year: 2010, course: 0)
+student.getCourse()
+
+
